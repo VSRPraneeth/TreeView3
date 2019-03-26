067643a57f190ce4c592480b99e68a82600e6b93
Praneeth <vprnth9@gmail.com>
2019-03-26 10:17:05 -0500

    Adding treeview3 files

---
+++ src/app/app.component.ts
@@ -0,0 +1,60 @@
+import { Component, enableProdMode } from '@angular/core';
+import { NestedTreeControl } from '@angular/cdk/tree';
+import { MatTreeNestedDataSource } from '@angular/material/tree';
+import { DataService } from './service/data.service';
+
+/**
+ * Food data with nested structure.
+ * Each node has a name and an optiona list of children.
+ */
+interface TreeNode {
+  name: string;
+  children?: TreeNode[];
+}
+
+//const TREE_DATA: TreeNode[] = [
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'TreeView';
+
+  treeControl = new NestedTreeControl<TreeNode>(node => node.children);
+  dataSource = new MatTreeNestedDataSource<TreeNode>();
+  chield: any;
+  values: string[];
+  TREE_DATA: TreeNode[];
+  selectdNodeName: String;
+  level : number =1;
+  
+  constructor(private dataservice: DataService) {
+    this.dataservice.getJSON().subscribe(data => {
+      console.log(data);
+      this.TREE_DATA = data;
+      this.dataSource.data = this.TREE_DATA;
+    });
+    
+    //this.dataSource.data = this.TREE_DATA;
+  }
+
+  hasChild = (_: number, node: TreeNode) => !!node.children && node.children.length > 0;
+
+  logNode(node) {
+
+    this.level = this.level + 1 ;
+    this.selectdNodeName = node.name;
+    if (node.children[1].children == null) {
+      this.values = node.children;
+    }
+    else {
+      this.values = null;
+    }
+    //this.values=node.children;
+    //console.log(this.values);
+  }
+}
+enableProdMode();
---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTreeModule, MatIconModule, MatButtonModule, MatChipsModule, MatLabel, MatFormFieldModule, MatTabsModule } from '@angular/material';
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CardModule } from 'primeng/card';
+import { AccordionModule } from 'primeng/accordion';
+import { ChipsModule } from 'primeng/chips';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatTreeModule,
+    FormsModule,
+    MatIconModule,
+    HttpClientModule,
+    CardModule,
+    ChipsModule,
+    AccordionModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
+    MatChipsModule,
+    MatFormFieldModule,
+    MatTabsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
